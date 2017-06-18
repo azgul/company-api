e5cc19b71d59881285f616c2e43394cfea9d22ee
Martin Kristensen <me@azgul.com>
2017-06-18 10:19:46 +0200

    Test that company creation works

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can create a new company' do
+    company = Company.new(name: 'A cool company', cvr: 12345678, address: 'A cool address', city: 'Aarhus', country: 'Denmark', phone: '+45 12 34 56 78')
+
+    company.save!
+
+    saved_company = Company.find(company.id)
+
+    expect(saved_company.name).to eq('A cool company')
+    expect(saved_company.cvr).to eq(12345678)
+    expect(saved_company.address).to eq('A cool address')
+    expect(saved_company.city).to eq('Aarhus')
+    expect(saved_company.country).to eq('Denmark')
+    expect(saved_company.phone).to eq('+45 12 34 56 78')
+  end
 end
